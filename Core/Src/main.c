f16fef6edaee31bf74bda1fc18ee0d5454804a03
Samir Gattás <samirgattas@gmail.com>
2020-09-09 18:33:17 -0300

    Nuevos archivos ps2_controller.c y .h. Interfaz para la logica de lo que se hace con el control PS2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,6 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "PS2X_lib.h"
 #include "ps2_controller.h"
 /* USER CODE END Includes */
 
