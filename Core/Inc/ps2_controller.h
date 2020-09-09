1c236f03282f3a54cab76aba68775fbc64586845
Samir Gattás <samirgattas@gmail.com>
2020-09-09 18:26:37 -0300

    Archivos ps2_controller.c y .h. Van todas las funciones relacionadas al control PS2

---
+++ Core/Inc/ps2_controller.h
@@ -0,0 +1,19 @@
+/*
+ * ps2_controller.h
+ *
+ *  Created on: 9 sept. 2020
+ *      Author: xx
+ */
+
+#ifndef INC_PS2_CONTROLLER_H_
+#define INC_PS2_CONTROLLER_H_
+
+#include "main.h"
+#include "PS2X_lib.h"
+
+
+void conectar_ps2();
+void acusar_botones();
+
+
+#endif /* INC_PS2_CONTROLLER_H_ */
